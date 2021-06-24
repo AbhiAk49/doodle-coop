9b8e61e6c6648c6a3fbc01d96a18b022e2a096b1
Abhishek Kumar <Abhiak49kumar@gmail.com>
2021-06-24 18:40:45 +0530

    added create session card

--- src/components/AppHome.vue
+++ src/components/AppHome.vue
@@ -15,12 +15,44 @@
         </v-col>
       </v-row>
       <v-row v-else>
-        loaded
+        <v-container fluid class="my-5">
+          <v-card
+          class="mx-0 pa-2"
+          elevation="3"
+          outlined
+          shaped
+        >
+          <v-img class="white--text align-end"
+                aspect-ratio = "1.778"
+                height="100px"
+                 src="../assets/foliage.png">
+          <v-card-title>Create a new Session</v-card-title>
+          </v-img>
+          <v-card-subtitle>Add Users to new session</v-card-subtitle>
+          <v-divider></v-divider>
+          <v-col cols="12">
+            <v-autocomplete
+              v-model="selectedUsers"
+              :items="users"
+              dense
+              chips
+              small-chips
+              label="Search for registered users"
+              multiple
+              solo
+            ></v-autocomplete>
+          <v-btn color="primary" @click="startSession">
+            START
+          </v-btn>
+        </v-col>
+        </v-card>
+        </v-container>
       </v-row>
     </v-container>
 </template>
 
 <script>
+import {getUsers} from '@/services/auth';
 import {fetchUserSessions} from '@/services/sessions';
   export default {
     name:'AppHome',
@@ -32,14 +64,19 @@ import {fetchUserSessions} from '@/services/sessions';
           name:String,
           email:String,
           token:String
-      }
+      },
+      users:[],
+      selectedUsers:[]
 
     }),
     computed: {
               token(){
             //console.log(this.$store.state.auth.token);
             return this.$store.state.auth.token;
-        }
+            },
+            getName(){
+                return this.$store.state.auth.name;
+            }
     },
     methods: {
       fetch(){
@@ -55,9 +92,16 @@ import {fetchUserSessions} from '@/services/sessions';
                         this.$router.push({name:'login'});
                     });
         },
+        startSession(){
+          console.log(this.selectedUsers);
+        }
     },
     created(){
       this.fetch();
+      getUsers(this.token)
+                  .then(response=>{
+                    this.users=response.map(user=>user.name).filter(name=>name!==this.getName);
+                  })
     }
   }
 </script>
